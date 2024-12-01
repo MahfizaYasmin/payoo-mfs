5f3a50eeda067407fcd61b8294a2b6f2811ef525
MahfizaYasmin <mahfizayasmin@gmail.com>
2024-12-02 01:06:52 +0200

    added partially the add money event handler

---
+++ js/home.js
@@ -0,0 +1,20 @@
+// add money to the account
+/*
+s-1: add event handler
+prevent page reload after form submit
+s-2: get money to be added to the account balance
+*/ 
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // s-2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput)
+});
